3cb550927a7f0ca4466af5c19b114b086cdf3b17
Greg Taylor <snagglepants@gmail.com>
2013-11-21 19:38:35 -0500

    Removing defunct france-2 relay

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ config.relays = ['tcp://master.eve-emdr.com:8050',
                  'tcp://relay-us-east-1.eve-emdr.com:8050',
                  'tcp://relay-ca-east-1.eve-emdr.com:8050',
                  'tcp://relay-eu-germany-1.eve-emdr.com:8050',
-                 'tcp://relay-eu-france-2.eve-emdr.com:8050',
                  'tcp://relay-eu-denmark-1.eve-emdr.com:8050'];
 
 // EMDR Reference relay
